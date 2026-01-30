87659a013d6edcd69bcf0888dc612070a8b542a5
fabian <fabianherrera8080@@gmail.com>
2026-01-30 12:05:28 -0500

    add missing annotation

--- src/main/java/com/arrive/invoiceservice/repository/ProductRepository.java
+++ src/main/java/com/arrive/invoiceservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.arrive.invoiceservice.repository;
 
 import com.arrive.invoiceservice.repository.entity.invoice.ProductEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository <ProductEntity, String>{
 }
